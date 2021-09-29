488c542156958c118063d8549f8fb2a956b6193c
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-09-30 01:21:23 +0700

    menu create

--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,30 +1,83 @@
 import { Component, OnInit } from '@angular/core';
-import {Car} from '../_model/car-model'
-import {CarserviceService} from './carservice.service'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Car } from '../_model/car-model';
+import { CarserviceService } from './carservice.service';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
-
   listCar: Array<Car>;
+  isShowmodal: boolean;
+  licensePate: string;
+  annotation: string;
+  modelCar: string;
+  brandCar: string;
+  validateForm!: FormGroup;
+  isConfirmLoading:boolean;
 
-
-  constructor( private apicall : CarserviceService) {
-
-    this.listCar = [] as Array<Car>
+  constructor(private apicall: CarserviceService, private fb: FormBuilder) {
+    this.listCar = [] as Array<Car>;
+    this.isShowmodal = false;
+    this.licensePate = '';
+    this.annotation = '';
+    this.modelCar = '';
+    this.brandCar = '';
+    this.isConfirmLoading = false;
   }
 
   ngOnInit() {
-
-    this.apicall.getCar().then((res)=>{
-
+    this.apicall.getCar().then((res) => {
       this.listCar = res.data;
       console.log(res);
-    })
+    });
 
+    this.validateForm = this.fb.group({
+      note: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
 
+    });
   }
 
+  genderChange = (value: string): void => {
+    this.validateForm
+      .get('note')!
+      .setValue(value === 'male' ? 'Hi, man!' : 'Hi, lady!');
+  };
+
+  submitForm = (): void => {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+
+    if (this.validateForm.valid) {
+      console.log("pass");
+
+      this.submitdata();
+    }
+  };
+
+  cancel = (): void => {
+    this.isShowmodal = false;
+  };
+  submitdata = (): void => {
+    var e = '';
+
+    this.validateForm.controls.note.setValue(e);
+
+    // this.validateForm.get('note')?.setValue(value === '');
+
+    this.isShowmodal = false;
+
+    // api call
+  };
+  addData = (): void => {
+    this.isShowmodal = true;
+
+    console.log('click');
+  };
 }
